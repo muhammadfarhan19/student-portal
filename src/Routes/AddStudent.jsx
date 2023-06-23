dd4ec0b806f0b21e4d90d96cb935ddd15acd363b
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2023-06-23 13:16:09 +0700

    styling add page

--- src/Routes/AddStudent.jsx
+++ src/Routes/AddStudent.jsx
@@ -5,13 +5,15 @@ import {
   Container,
   Center,
   Text,
+  Grid,
+  Box,
   FormControl,
   FormLabel,
   Input,
   Select,
   Button,
 } from "@chakra-ui/react";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 // import { addStudent } from "../api";
 
 const AddStudent = () => {
@@ -54,11 +56,6 @@ const AddStudent = () => {
   };
   const faculty = getFaculty(programStudy);
 
-  // const addData = async () => {
-
-  //   const data = await response.json();
-  // }
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newData = {
@@ -85,73 +82,177 @@ const AddStudent = () => {
   };
 
   return (
-    <Container maxW="100%" padding="100px" height="100vh" bg="green.100">
-      <Center flexDirection="column" bg="green.200" height='100%'>
-        <Text align="start">Add Student</Text>
-        <FormControl width="50%" onSubmit={handleSubmit}>
-          <FormLabel>Fullname</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => setFullname(e.target.value)}
-            data-testid="name"
-          />
-          <FormLabel>Profile Picture</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => setProfilePicture(e.target.value)}
-            data-testid="profilePicture"
-          />
-          <FormLabel>Address</FormLabel>
-          <Input
-            type="text"
-            onChange={(e) => setAddress(e.target.value)}
-            data-testid="address"
-          />
-          <FormLabel>Phone Number</FormLabel>
-          <Input
-            type="number"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            data-testid="phoneNumber"
-          />
-          <FormLabel>Birthdate</FormLabel>
-          <Input
-            type="date"
-            onChange={(e) => setBirthdate(e.target.value)}
-            data-testid="date"
-          />
-          <FormLabel>Gender</FormLabel>
-          <Select
-            onChange={(e) => setGender(e.target.value)}
-            data-testid="gender"
-          >
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </Select>
-          <FormLabel>Program Study</FormLabel>
-          <Select
-            onChange={(e) => setProgramStudy(e.target.value)}
-            data-testid="prody"
-          >
-            <option value="Ekonomi">Ekonomi</option>
-            <option value="Manajemen">Manajemen</option>
-            <option value="Akuntansi">Akuntansi</option>
-            <option value="Administrasi Publik">Administrasi Publik</option>
-            <option value="Administrasi Bisnis">Administrasi Bisnis</option>
-            <option value="Hubungan Internasional">
-              Hubungan Internasional
-            </option>
-            <option value="Teknik Sipil">Teknik Sipil</option>
-            <option value="Arsitektur">Arsitektur</option>
-            <option value="Matematika">Matematika</option>
-            <option value="Fisika">Fisika</option>
-            <option value="Informatika">Informatika</option>
-          </Select>
-          <Button marginTop="20px" onClick={handleSubmit} data-testid="add-btn">
-            Add student
+    // <Container maxW="100%" padding="100px" height="100vh" bg="green.100">
+    //   <Center flexDirection="column" bg="green.200" height="100%">
+    //     <Text align="start">Add Student</Text>
+    //     <FormControl
+    //       width="50%"
+    //       onSubmit={handleSubmit}
+    //       display="flex"
+    //       flexDirection="column"
+    //       flexWrap="wrap"
+    //     >
+    //       <FormLabel>Fullname</FormLabel>
+    //       <Input
+    //         type="text"
+    //         onChange={(e) => setFullname(e.target.value)}
+    //         data-testid="name"
+    //       />
+    //       <FormLabel>Profile Picture</FormLabel>
+    //       <Input
+    //         type="text"
+    //         onChange={(e) => setProfilePicture(e.target.value)}
+    //         data-testid="profilePicture"
+    //       />
+    //       <FormLabel>Address</FormLabel>
+    //       <Input
+    //         type="text"
+    //         onChange={(e) => setAddress(e.target.value)}
+    //         data-testid="address"
+    //       />
+    //       <FormLabel>Phone Number</FormLabel>
+    //       <Input
+    //         type="number"
+    //         onChange={(e) => setPhoneNumber(e.target.value)}
+    //         data-testid="phoneNumber"
+    //       />
+    //       <FormLabel>Birthdate</FormLabel>
+    //       <Input
+    //         type="date"
+    //         onChange={(e) => setBirthdate(e.target.value)}
+    //         data-testid="date"
+    //       />
+    //       <FormLabel>Gender</FormLabel>
+    //       <Select
+    //         onChange={(e) => setGender(e.target.value)}
+    //         data-testid="gender"
+    //       >
+    //         <option value="Male">Male</option>
+    //         <option value="Female">Female</option>
+    //       </Select>
+    //       <FormLabel>Program Study</FormLabel>
+    //       <Select
+    //         onChange={(e) => setProgramStudy(e.target.value)}
+    //         data-testid="prody"
+    //       >
+    //         <option value="Ekonomi">Ekonomi</option>
+    //         <option value="Manajemen">Manajemen</option>
+    //         <option value="Akuntansi">Akuntansi</option>
+    //         <option value="Administrasi Publik">Administrasi Publik</option>
+    //         <option value="Administrasi Bisnis">Administrasi Bisnis</option>
+    //         <option value="Hubungan Internasional">
+    //           Hubungan Internasional
+    //         </option>
+    //         <option value="Teknik Sipil">Teknik Sipil</option>
+    //         <option value="Arsitektur">Arsitektur</option>
+    //         <option value="Matematika">Matematika</option>
+    //         <option value="Fisika">Fisika</option>
+    //         <option value="Informatika">Informatika</option>
+    //       </Select>
+    //       <Button marginTop="20px" onClick={handleSubmit} data-testid="add-btn">
+    //         Add student
+    //       </Button>
+    //     </FormControl>
+    //   </Center>
+    //   <Footer />
+    // </Container>
+    <Container bg="blue.100" maxW="100%" height="100vh" paddingTop={20} textAlign='center' >
+      {/* <Center> */}
+      <Text fontSize="24px" fontWeight='bold' >Add student</Text>
+      <Box maxWidth="700px" mx="auto" bg="green.200" marginTop={8}>
+        <form onSubmit={handleSubmit}>
+          <Grid templateColumns="1fr" gap={4} mb={4}>
+            <FormControl>
+              <FormLabel>Fullname</FormLabel>
+              <Input
+                type="text"
+                onChange={(e) => setFullname(e.target.value)}
+                data-testid="name"
+              />
+            </FormControl>
+          </Grid>
+          <Grid templateColumns="1fr" gap={4} mb={4}>
+            <FormControl>
+              <FormLabel>Profile picture</FormLabel>
+              <Input
+                type="text"
+                onChange={(e) => setProfilePicture(e.target.value)}
+                data-testid="profilePicture"
+              />
+            </FormControl>
+          </Grid>
+          <Grid templateColumns="1fr" gap={4} mb={4}>
+            <FormControl>
+              <FormLabel>Address</FormLabel>
+              <Input
+                type="text"
+                onChange={(e) => setAddress(e.target.value)}
+                data-testid="address"
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid templateColumns="repeat(2, 1fr)" gap={4} mb={4}>
+            <FormControl>
+              <FormLabel>Phone number</FormLabel>
+              <Input
+                type="number"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                data-testid="phoneNumber"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Birthdate</FormLabel>
+              <Input
+                type="date"
+                onChange={(e) => setBirthdate(e.target.value)}
+                data-testid="date"
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid templateColumns="1fr 1fr" gap={4} mb={4}>
+            <FormControl>
+              <FormLabel>Gender</FormLabel>
+              <Select
+                onChange={(e) => setGender(e.target.value)}
+                data-testid="gender"
+              >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </Select>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Program study</FormLabel>
+              <Select
+                onChange={(e) => setProgramStudy(e.target.value)}
+                data-testid="prody"
+              >
+                <option value="Ekonomi">Ekonomi</option>
+                <option value="Manajemen">Manajemen</option>
+                <option value="Akuntansi">Akuntansi</option>
+                <option value="Administrasi Publik">Administrasi Publik</option>
+                <option value="Administrasi Bisnis">Administrasi Bisnis</option>
+                <option value="Hubungan Internasional">
+                  Hubungan Internasional
+                </option>
+                <option value="Teknik Sipil">Teknik Sipil</option>
+                <option value="Arsitektur">Arsitektur</option>
+                <option value="Matematika">Matematika</option>
+                <option value="Fisika">Fisika</option>
+                <option value="Informatika">Informatika</option>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Button colorScheme="blue" type="submit"  onClick={handleSubmit} data-testid="add-btn">
+            Submit
           </Button>
-        </FormControl>
-      </Center>
-      <Footer />
+        </form>
+      </Box>
+      {/* </Center> */}
     </Container>
   );
 };
